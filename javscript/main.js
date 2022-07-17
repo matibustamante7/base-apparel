5fb85c0a17ae0114dcc203dee86bd321fa1efeab
matibustamante7 <matiasebustamante7@gmail.com>
2022-07-17 18:54:29 -0300

    primera subida

---
+++ javscript/main.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('.container__content__form');
+const inputEmail = document.querySelector('.container__content__form__email');
+const error = document.querySelector('.container__content__form__email--error');
+
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+    if(regExp.test(inputEmail.value) == true){
+        inputEmail.style.border = '1px solid hsl(0, 36%, 70%)';
+        inputEmail.style.backgroundImage = "url('')";
+        error.innerHTML = '';
+        
+    }else{
+        inputEmail.style.border = '2px solid red';
+        inputEmail.style.backgroundImage = "url('./images/icon-error.svg')";
+        error.innerHTML = 'Please provide a valir email!';
+    }
+});
